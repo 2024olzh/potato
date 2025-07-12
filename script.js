4f4b24343bd2682efc588d3919ed1af60dd832ac
2024olzh <2024olzh@gmail.com>
2025-07-12 12:14:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file can be used for custom JavaScript functionalities.
+// Currently, there are no specific JavaScript interactions required for this page.
+// Example: You could add a function here to handle button clicks or dynamic content loading.
+
+// console.log("Workshop page JavaScript loaded!");
